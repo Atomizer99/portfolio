63805ba74af6e8c72dd5a9008ad219523320a69f
Atomizer99 <eli.kaplan70@gmail.com>
2021-08-10 20:58:17 +0300

    succesful mongo connection to app.proceed to docker compose.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask,request,render_template, redirect ,url_for, send_from_directory,Response,jsonify
-#from flask_pymongo import PyMongo
-
-#app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-#mongo = PyMongo(app)
+from flask import Flask,request,render_template,jsonify
+from flask_pymongo import pymongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('mongodb://localhost:27017/')
+db = client.calocalc_database
+collection = db.calocalc_collection
+users = db.users
+
+
 
 @app.route('/calculate', methods=["POST"])
 def BMRCalc():
@@ -21,8 +25,19 @@ def BMRCalc():
     elif Sex == "Female":
         BMR = (10 * Weight) + (6.25 * Height) - (5 * Age) - 161
         cals = BMR * 1.2
+    user = {
+        "firstname": request.form.get('fname'),
+        "lastname": request.form.get('lname'),
+        "age": request.form.get('Age'),
+        "sex": request.form.get('Sex'),
+        "height": request.form.get('Height'),
+        "weight": request.form.get('Weight'),
+        "BMR": BMR,
+        "cals": cals
+    }
+    user_id = users.insert_one(user)
     return "Your BMR is: " + str(BMR) + "." + "<br>Your daily calorie intake should be at least: " + str(cals) + "."
-
+    
 @app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
