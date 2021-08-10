a9453abc1baa5be7796eb8a74b29e2dbd1596245
Atomizer99 <eli.kaplan70@gmail.com>
2021-08-10 15:10:33 +0300

    MBRCAL methos was built

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ from flask import Flask,request,render_template, redirect ,url_for, send_from_di
 
 app = Flask(__name__)
 
-def calorieCalc():
+@app.route('/calculate', methods=["POST"])
+def BMRCalc():
+    Sex = request.args.get('Sex')
+    Height = request.args.get('Height')
+    Weight = request.args.get('Weight')
+    Age = request.args.get('Age')
+    if Sex == "Male":
+        BMR = (10 * Weight) + (6.25 * Height) - (5 * Age) + 5
+        cals = BMR * 1.2
+    if Sex == "Female":
+        BMR = (10 * Weight) + (6.25 * Height) - (5 * Age) - 161
+        cals = BMR * 1.2
+    return "Your BMR is" + BMR + "\nYour daily calorie intake shoudl be at least" + cal + "."
 
-@app.route("/")
+@app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
