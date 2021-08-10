9081e7275741cf778a94374d63f0c5cfaa5d59d2
Atomizer99 <eli.kaplan70@gmail.com>
2021-08-10 18:36:28 +0300

    added nginx.conf.working fine.

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 from flask import Flask,request,render_template, redirect ,url_for, send_from_directory,Response,jsonify
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-mongo = PyMongo(app)
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+#mongo = PyMongo(app)
 
 app = Flask(__name__)
 
