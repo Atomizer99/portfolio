e8b52b855eeac8f6955ea584321806d26878fa67
Atomizer99 <eli.kaplan70@gmail.com>
2021-08-10 11:04:56 +0300

    added base.html file

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask,request,render_template, redirect ,url_for, send_from_di
 
 app = Flask(__name__)
 
+def calorieCalc():
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
