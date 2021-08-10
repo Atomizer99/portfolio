b5a196adb452ac2c18bc997b8888bcc63cc4a584
Atomizer99 <eli.kaplan70@gmail.com>
2021-08-10 15:51:17 +0300

    App is working fine

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask,request,render_template, redirect ,url_for, send_from_directory,Response,jsonify
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-mongo = PyMongo(app)
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+#mongo = PyMongo(app)
 
 app = Flask(__name__)
 
 @app.route('/calculate', methods=["POST"])
 def BMRCalc():
-    Sex = request.args.get('Sex')
-    Height = request.args.get('Height')
-    Weight = request.args.get('Weight')
-    Age = request.args.get('Age')
+    Sex = request.form.get('Sex')
+    Height = float(request.form.get('Height'))
+    Weight = float(request.form.get('Weight'))
+    Age = int(request.form.get('Age'))
     BMR = 0
     cals = 0
     print(Sex)
@@ -21,7 +21,7 @@ def BMRCalc():
     elif Sex == "Female":
         BMR = (10 * Weight) + (6.25 * Height) - (5 * Age) - 161
         cals = BMR * 1.2
-    return "Your BMR is" + str(BMR) + "\nYour daily calorie intake shoudl be at least" + str(cals) + "."
+    return "Your BMR is: " + str(BMR) + "." + "<br>Your daily calorie intake shoudl be at least: " + str(cals) + "."
 
 @app.route('/', methods=["GET"])
 def index():
