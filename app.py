d0a2c8bd0f1b121b015045e01c0ba9580c2d7d29
Atomizer99 <eli.kaplan70@gmail.com>
2021-08-10 15:35:43 +0300

    added mongodb requirement to code

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask,request,render_template, redirect ,url_for, send_from_directory,Response,jsonify
+from flask_pymongo import PyMongo
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
 app = Flask(__name__)
 
@@ -8,13 +12,16 @@ def BMRCalc():
     Height = request.args.get('Height')
     Weight = request.args.get('Weight')
     Age = request.args.get('Age')
+    BMR = 0
+    cals = 0
+    print(Sex)
     if Sex == "Male":
         BMR = (10 * Weight) + (6.25 * Height) - (5 * Age) + 5
         cals = BMR * 1.2
-    if Sex == "Female":
+    elif Sex == "Female":
         BMR = (10 * Weight) + (6.25 * Height) - (5 * Age) - 161
         cals = BMR * 1.2
-    return "Your BMR is" + BMR + "\nYour daily calorie intake shoudl be at least" + cal + "."
+    return "Your BMR is" + str(BMR) + "\nYour daily calorie intake shoudl be at least" + str(cals) + "."
 
 @app.route('/', methods=["GET"])
 def index():
